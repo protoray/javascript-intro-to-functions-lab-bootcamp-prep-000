b1188dcf378e11592c36d08ab5d37ac3d9b9ac31
Ray <ray@protozo.net>
2018-02-12 06:38:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string)
+{
+ return string.toUpperCase() // 'STRING!'
+}
+
+function whisper(string)
+{
+  return string.toLowerCase() // 'STRING -> string'
+}
+
+function logShout(string)
+{
+  console.log `${string.toUpperCase}`
+}
